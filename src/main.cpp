838d5e18d372a68748c23388e572e6d7c4739d6f
nanax74 <nanax74m@gmail.com>
2026-07-02 12:59:20 +0900

    0.1.0

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <wups.h>
+#include <notifications/notifications.h>
+
+WUPS_PLUGIN_NAME("helloworld");
+WUPS_PLUGIN_AUTHOR("nanax74");
+WUPS_PLUGIN_DESCRIPTION("A plugin for testing the development environment of Aroma plugins.");
+WUPS_PLUGIN_VERSION("0.1.0");
+WUPS_PLUGIN_LICENSE("MIT");
+
+INITIALIZE_PLUGIN()
+{
+    NotificationModule_InitLibrary();
+    NotificationModule_AddInfoNotification("Hello World");
+}
+
+DEINITIALIZE_PLUGIN()
+{
+    NotificationModule_DeInitLibrary();
+}
